eba908c8138f16cc966509c0464c8710a3ded3a1
Moises Haniel Gutierrez Salazar <moisesgutierrez230397@gmail.com>
2026-09-19 12:16:58 -0600

    Nombre del estudiante registrado

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,7 +1,7 @@
 # ==========================================
 # PROYECTO FINAL: Sistema de Registro Académico
 # Asignatura: Programación Estructurada 2026
-# Estudiante: Moises Gutierrez Salazar
+# Estudiante: Moises Haniel Gutierrez Salazar
 # ==========================================
 
 # PASO 1: Estructura de Datos
